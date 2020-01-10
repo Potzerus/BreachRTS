9f121de496d554333492a583081d22b7a1d09d09
potzerus <Potzerus@gmail.com>
2020-01-10 19:40:15 +0100

    Thx Samuel

--- src/unit_test.rs
+++ src/unit_test.rs
@@ -3,7 +3,8 @@ extern crate image as im;
 extern crate vecmath;
 
 use piston_window::*;
-use vecmath::*;
+use vecmath::vec2_sub;
+//use vecmath::*;
 
 fn main() {
     let opengl = OpenGL::V3_2;
@@ -14,52 +15,65 @@ fn main() {
             .build()
             .unwrap();
 
-    let mut draw = false;
+    const GREEN:[f32;4] = [0.0, 1.0, 0.0, 1.0];
+    let mut is_pressed = false;
+
+    let mut cursor: Option<[f64; 2]> = None;
     let mut first_corner: Option<[f64; 2]> = None;
-    let mut current: Option<[f64; 2]> = None;
+    let mut live_corner: Option<[f64; 2]> = None;
+    let mut _distance: Option<[f64; 2]> = None;
     while let Some(e) = window.next() {
-        if let Some(_) = e.render_args() {
-            window.draw_2d(&e, |c, g, device| {
-                clear([1.0; 4], g);
-                if draw {
-                    if let Some(curr) = current {
-                        println!("{},{}", curr[0], curr[1]);
-                        rectangle([0.0, 1.0, 0.0, 1.0],
-                                  [curr[0], curr[1], curr[0] + 200.0, curr[1] + 200.0],
-                                  c.transform, g);
-                    }
-                };
-            });
+        if let Some(Button::Mouse(MouseButton::Left)) = e.press_args() {
+            is_pressed = true;
+            live_corner = cursor;
+            first_corner = cursor;
         }
-//        if let Some(button) = e.press_args() {
-//            if button == Button::Mouse(MouseButton::Left) {
-//                match e.mouse_cursor_args() {
-//                    Some(mca) => {
-//                        println!("{},{},{}", mca[0], mca[1], draw);
-//                        if !draw {
-//                            draw = true;
-//                            let mut holder = [0.0, 0.0];
-//                            holder[0] = mca[0];
-//                            holder[1] = mca[1];
-//                            first_corner = Some(holder);
-//                            }
-//                        }
-//                        None => {
-//                            println!("I AM NONE")
-//                        }
-//                }
-//            }
-//        };
-        //if let Some(button) = e.release_args() {
-        //    if button == Button::Mouse(MouseButton::Left) {
-        //        draw = false;
-        //        first_corner = None
-        //    }
-        //};
+        if let Some(button) = e.release_args() {
+            if button == Button::Mouse(MouseButton::Left) {
+                is_pressed = false;
+                live_corner = None
+            }
+        };
         if let Some(pos) = e.mouse_cursor_args() {
-            current = Some(pos);
-        } else {
-            println!("Cursor args is None")
+            cursor = Some(pos);
+
+            if is_pressed {
+                live_corner = Some(pos);
+            }
+        }
+
+        if let Some(_) = e.render_args() {
+            window.draw_2d(&e, |c, g, _device| {
+                clear([1.0; 4], g);
+                if is_pressed {
+                    if let Some(fc) = first_corner {
+                        if let Some(lc) = live_corner {
+                            let dist = vec2_sub(lc, fc);
+                            /*rectangle(GREEN,
+                                      [fc[0], fc[1], dist[0], dist[1]],
+                                      c.transform, g);*/
+                            line(GREEN,
+                                 1.0,
+                                 [fc[0], fc[1], fc[0], lc[1]],
+                                 c.transform, g);
+                            line(GREEN,
+                                 1.0,
+                                 [fc[0], fc[1], lc[0], fc[1]],
+                                 c.transform, g);
+                            line(GREEN,
+                                 1.0,
+                                 [fc[0], lc[1], lc[0], lc[1]],
+                                 c.transform, g);
+                            line(GREEN,
+                                 1.0,
+                                 [lc[0], fc[1], lc[0], lc[1]],
+                                 c.transform, g);
+                        }
+                    }
+                }
+            });
         }
     }
 }
+
+
