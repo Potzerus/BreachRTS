4c323e052e75c6f16b719afff922eebfd11243ce
potzerus <Potzerus@gmail.com>
2020-01-14 22:22:58 +0100

    Samuel Update

--- src/unit_test.rs
+++ src/unit_test.rs
@@ -6,28 +6,29 @@ use piston_window::*;
 use vecmath::vec2_sub;
 //use vecmath::*;
 
-const RED:[f32;4] = [1.0, 0.0, 0.0, 1.0];
-const GREEN:[f32;4] = [0.0, 1.0, 0.0, 1.0];
-const BLUE:[f32;4] = [0.0, 0.0, 1.0, 1.0];
-const COLOR_IRON:[f32;4] = [0.1, 0.1, 0.1, 1.0];
-const COLOR_FLESH:[f32;4] = [0.7, 0.0, 0.0, 1.0];
-const COLOR_WOOD:[f32;4] = [0.3, 0.0, 0.0, 1.0];
-const COLOR_GROUND:[f32;4] = [0.5, 0.5, 0.5, 1.5];
+const RED: [f32; 4] = [1.0, 0.0, 0.0, 1.0];
+const GREEN: [f32; 4] = [0.0, 1.0, 0.0, 1.0];
+const BLUE: [f32; 4] = [0.0, 0.0, 1.0, 1.0];
+const COLOR_IRON: [f32; 4] = [0.1, 0.1, 0.1, 1.0];
+const COLOR_FLESH: [f32; 4] = [0.7, 0.0, 0.0, 1.0];
+const COLOR_WOOD: [f32; 4] = [0.3, 0.0, 0.0, 1.0];
+const COLOR_GROUND: [f32; 4] = [0.5, 0.5, 0.5, 1.5];
 
 
 enum Tiles {
     Iron,
     Flesh,
     Wood,
-    Ground
+    Ground,
 }
-impl Tiles{
-    fn get_color(&self)->[f32;4]{
+
+impl Tiles {
+    fn get_color(&self) -> [f32; 4] {
         match self {
-            Tiles::Iron=> COLOR_IRON,
+            Tiles::Iron => COLOR_IRON,
             Tiles::Flesh => COLOR_FLESH,
             Tiles::Wood => COLOR_WOOD,
-            Tiles::Ground=> COLOR_GROUND,
+            Tiles::Ground => COLOR_GROUND,
         }
     }
 }
@@ -73,32 +74,32 @@ fn main() {
             window.draw_2d(&e, |c, g, _device| {
                 clear([1.0; 4], g);
                 if is_pressed {
-                    if let Some(fc) = first_corner {
-                        if let Some(lc) = live_corner {
-                            let dist = vec2_sub(lc, fc);
-                            /*rectangle(GREEN,
-                                      [fc[0], fc[1], dist[0], dist[1]],
-                                      c.transform, g);*/
-                            line(GREEN,
-                                 1.0,
-                                 [fc[0], fc[1], fc[0], lc[1]],
-                                 c.transform, g);
-                            line(GREEN,
-                                 1.0,
-                                 [fc[0], fc[1], lc[0], fc[1]],
-                                 c.transform, g);
-                            line(GREEN,
-                                 1.0,
-                                 [fc[0], lc[1], lc[0], lc[1]],
-                                 c.transform, g);
-                            line(GREEN,
-                                 1.0,
-                                 [lc[0], fc[1], lc[0], lc[1]],
-                                 c.transform, g);
-                        }
-                    }
+                    selection_from_points(c, g, first_corner, live_corner);
                 }
             });
+        }
+    }
+}
+
+fn selection_from_points(c: Context, g: &mut G2d, first_corner: [f64; 2], live_corner: [f64; 2]) {
+    if let Some(fc) = first_corner {
+        if let Some(lc) = live_corner {
+            line(GREEN,
+                 1.0,
+                 [fc[0], fc[1], fc[0], lc[1]],
+                 c.transform, g);
+            line(GREEN,
+                 1.0,
+                 [fc[0], fc[1], lc[0], fc[1]],
+                 c.transform, g);
+            line(GREEN,
+                 1.0,
+                 [fc[0], lc[1], lc[0], lc[1]],
+                 c.transform, g);
+            line(GREEN,
+                 1.0,
+                 [lc[0], fc[1], lc[0], lc[1]],
+                 c.transform, g);
         }
     }
 }
